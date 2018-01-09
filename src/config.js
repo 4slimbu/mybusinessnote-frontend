8ab29c5c44fe11f3c22e6477033fe36a9b923efc
Sudip Limbu <sudiplimburp@gmail.com>
2018-01-09 17:56:16 +0545

    added base url for staging

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL = "http://mbj.dev/api";
+// export const API_BASE_URL = "http://mbj.dev/api";
+export const API_BASE_URL = "http://api.mybusinessjourney.com.au/api";
 // export const API_BASE_URL = "http://clients.responsive-pixel.com/mbj/public/api";
