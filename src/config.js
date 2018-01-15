5b40cd318e8088c9e46bf2132371919b2c6e0966
Sudip Limbu <4slimbu@gmail.com>
2018-01-15 20:49:32 +0545

    --amend

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://mbj.dev/api";
+// export const API_BASE_URL = "http://mbj.dev/api";
 // export const API_BASE_URL = "http://api.mybusinessjourney.com.au/api";
-// export const API_BASE_URL = "http://clients.responsive-pixel.com/mbj/public/api";
+export const API_BASE_URL = "http://clients.resposive-pixel.com/mbj/public/api";
