3db74ef9d37bd0ffde1ef000c58503d7c03b2a33
Sudip Limbu <sudiplimburp@gmail.com>
2018-01-29 08:37:24 +0545

    added google site ownership meta verification

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-// export const APP_ENV = "production";
-export const APP_ENV = "debug";
+export const APP_ENV = "production";
+// export const APP_ENV = "debug";
 export const APP_DEBUG_LEVEL = "debug";
 // export const API_BASE_URL = "http://mbj.dev/api";
 // export const API_BASE_IMAGE_URL = "http://mbj.dev";
-export const API_BASE_URL = "http://clients.responsive-pixel.com/mbj/public/api";
-export const API_BASE_IMAGE_URL = "http://clients.responsive-pixel.com/mbj/public";
-// export const API_BASE_URL = "http://api.mybusinessjourney.com.au/api";
-// export const API_BASE_IMAGE_URL = "http://api.mybusinessjourney.com.au";
+// export const API_BASE_URL = "http://clients.responsive-pixel.com/mbj/public/api";
+// export const API_BASE_IMAGE_URL = "http://clients.responsive-pixel.com/mbj/public";
+export const API_BASE_URL = "http://api.mybusinessjourney.com.au/api";
+export const API_BASE_IMAGE_URL = "http://api.mybusinessjourney.com.au";
