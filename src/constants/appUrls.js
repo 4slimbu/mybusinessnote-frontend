f7b47a15e5238972b0150426e6cbfbf1e6dd0e3a
Sudip Limbu <sudiplimburp@gmail.com>
2018-02-27 07:51:08 +0545

    Organize constants in the application

---
+++ src/constants/appUrls.js
@@ -0,0 +1,6 @@
+export const LEVEL_ONE_URL = "/level/getting-started";
+export const LEVEL_TWO_URL = "/level/setting-the-foundations";
+export const LEVEL_THREE_URL = "/level/building-up-a-business";
+export const BUSINESS_CATEGORIES_URL = "/level/getting-started/section/business-category";
+export const SELL_GOODS_URL = "/level/getting-started/section/business-category/bo/2";
+export const ABOUT_YOU_URL = "/level/getting-started/section/about-you";
---
+++ src/constants/errorCodes.js
@@ -0,0 +1,30 @@
+//Form Validation Error Codes
+export const ERR_ABN_INVALID = "ABN is invalid";
+export const ERR_ABN_REQUIRED = "ABN is required";
+export const ERR_BUSINESS_NAME_REQUIRED = "Business name is required";
+export const ERR_EMAIL_ALREADY_EXIST = "User with given email is already registered";
+export const ERR_EMAIL_INVALID = "Invalid Email";
+export const ERR_EMAIL_REQUIRED = "Email is required";
+export const ERR_FIRST_NAME_REQUIRED = "First Name is required";
+export const ERR_LAST_NAME_REQUIRED = "Last Name is required";
+export const ERR_PASSWORD_CONFIRM_NOT_MATCHING = "Password and Confirm Password must match";
+export const ERR_PASSWORD_INVALID = "Password is invalid";
+export const ERR_PASSWORD_REQUIRED = "Password is required";
+export const ERR_PHONE_NUMBER_REQUIRED = "Phone Number is required";
+export const ERR_PHONE_NUMBER_INVALID = "Phone Number is invalid";
+export const ERR_URL_INVALID = "Url is invalid";
+export const ERR_URL_REQUIRED = "Url is required";
+
+//Login Error Codes
+export const ERR_INVALID_CREDENTIALS = "Invalid Credentials";
+export const ERR_TOKEN_EXPIRED = "User has been logged out due to inactivity";
+export const ERR_TOKEN_INVALID = "Token invalid";
+export const ERR_TOKEN_NOT_PROVIDED = "Token not provided";
+export const ERR_TOKEN_USER_NOT_FOUND = "User not found";
+
+//Page Error Codes
+export const ERR_NOT_FOUND = "Couldn't find the page";
+export const ERR_RESTRICTED = "You don't have enough permission to view this page";
+
+//Unknown Error Codes
+export const ERR_UNKNOWN = "Something went wrong";
