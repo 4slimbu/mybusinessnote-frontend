1ffe58f9b444ac670bec9ccfca6f01a3f76efd53
Sudip Limbu <4slimbu@gmail.com>
2018-03-15 00:10:04 +0545

    Continue Refactor

--- src/components/business-option/elements/old/RegisterBusiness.js
+++ src/components/business-option/elements/old/RegisterBusiness.js
@@ -2,147 +2,168 @@ import React, {Component} from 'react';
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {addFlashMessage} from "../../../../actions/flashMessageAction";
-import jwt_decode from "jwt-decode";
-import setAuthorizationToken from "../../../../utils/axios/setAuthorizationToken";
 import TextFieldGroup from "../../../common/TextFieldGroup";
 import {withRouter} from "react-router-dom";
+import request from "../../../../services/request";
+import {makeRequest} from "../../../../actions/requestAction";
+import {validateFields} from "../../../../utils/validation/FieldValidator";
+import {ROUTES} from "../../../../constants/routes";
 
 class RegisterBusiness extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            type: "update_business",
-            business_id: null,
-            abn: "sdfds",
+            abn: {
+                isChanged: false,
+                label: "Enter your Australian Business Number (ABN) *",
+                name: "abn",
+                placeholder: "eg. 3522 2525 2524",
+                value: "",
+                oldValue: "",
+                type: "text"
+            },
             errors: {},
-            isLoading: false
+            isChanged: false,
         };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
 
     componentDidMount() {
+        const {appStatus} = this.props;
+        const {business} = appStatus;
+
         this.setState({
-            business_option_id: this.props.appStatus.currentBusinessOption.id,
-            business_id: this.props.appStatus.business_id,
-            abn: this.props.appStatus.abn,
+            ...this.state,
+            abn: {
+                ...this.state.abn,
+                oldValue: business.abn,
+            },
         });
     }
 
-    componentWillReceiveProps() {
+    resetFields() {
         this.setState({
-            business_option_id: this.props.appStatus.currentBusinessOption.id,
-            business_id: this.props.appStatus.business_id,
-            abn: this.props.appStatus.abn,
-        });
+            abn: {
+                ...this.state.abn,
+                isChanged: false,
+                value: ''
+            },
+            errors: {},
+            isChanged: false,
+        })
     }
 
     onChange(e) {
+
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: {
+                ...this.state[e.target.name],
+                value: e.target.value,
+                isChanged: true
+            },
+            isChanged: true
+        }, function () {
+            const dataObject = {};
+            if (this.state.abn.isChanged) {
+                dataObject.abn = this.state.abn.value
+            }
+            this.isFormValid(dataObject);
         });
+
+
     }
 
-    isFormValid(data = null) {
-        let input = (data) ? data : this.state;
-        const {errors, isValid} = {errors: {}, isValid: true};
+    isFormValid(dataObject) {
+        const rules = {
+            abn: 'required|abn',
+        };
+        const {errors, isValid} = validateFields(dataObject, rules);
 
-        if (!isValid) {
-            this.setState({errors});
-        }
+        this.setState({
+            errors: isValid ? {} : errors
+        });
 
         return isValid;
     }
 
     onSubmit(e) {
         e.preventDefault();
-        const appStatus = this.props.appStatus;
-        if (this.isFormValid()) {
-            this.setState({
-                errors: {},
-                isLoading: true,
-            });
+        const {appStatus, makeRequest, history} = this.props;
 
-            if (!appStatus.user_id || !appStatus.business_id) {
-                this.props.addFlashMessage({
-                    type: "error",
-                    text: "You need to create an account before creating business"
-                });
-                return;
-            }
+        if (appStatus.business.id && !this.state.isChanged) {
+            history.push(ROUTES.REGISTER_ABN);
+        }
 
-            this.props.saveBusinessFormRequest(this.state, '/level/1/section/4/business-option/5').then(
-                (response) => {
-                    this.setState({isLoading: false});
-                    const token = response.data.token;
-                    if (token) {
-                        localStorage.setItem("jwtToken", token);
-                        setAuthorizationToken(token);
-                        this.props.setCurrentUser(jwt_decode(token).user);
-                    }
-
-                    const {
-                        setCurrentLevel, setCurrentSection, setCurrentBusinessOption, setCompletedStatus,
-                        history, getBusinessOption
-                    } = this.props;
-                    this.props.setCompletedStatus(response.data.completed_status);
-                    if (response.data.completed_status.level) {
-                        this.props.getAppStatus();
-                        this.props.addFlashMessage({
-                            type: "success",
-                            text: "Saved successfully!"
+        // new data
+        if (! appStatus.business.id) {
+            const newData = {
+                abn: this.state.abn.value,
+                business_option_id: appStatus.currentBusinessOption.id,
+            };
+            if (this.isFormValid(newData)) {
+                makeRequest(request.business.save, newData).then(
+                    (responseData) => {
+                        history.push(ROUTES.REGISTER_ABN);
+                    },
+                    (errorData) => {
+                        this.resetFields();
+                        this.setState({
+                            errors: errorData.errors ? errorData.errors : {}
                         });
-                        getBusinessOption(
-                            '/level/1/section/4/business-option/5',
-                            true);
-                        history.push('/level/getting-started');
-                    } else {
-                        setCurrentLevel(appStatus.levels[1]);
-                        setCurrentSection({});
-                        setCurrentBusinessOption({});
-                        setCompletedStatus({});
-                        history.push('/level/setting-the-foundations');
                     }
-                },
-                (error) => {
-                    this.setState({errors: error.response.data.error, isLoading: false});
-                    this.props.addFlashMessage({
-                        type: "error",
-                        text: "Failed!"
-                    });
+                );
+            }
+        }
+
+        //changed data
+        if (appStatus.business.id && this.state.isChanged) {
+            const changedData = {
+                business_option_id: appStatus.currentBusinessOption.id,
+            };
+            if (this.state.abn.isChanged) {
+                changedData.abn = this.state.abn.value
+            }
+            if (this.isFormValid(changedData)) {
+                if (this.isFormValid(changedData)) {
+                    makeRequest(request.Business.save, changedData).then(
+                        (responseData) => {
+                            history.push(ROUTES.REGISTER_ABN);
+                        },
+                        (errorData) => {
+                            this.resetFields();
+                            this.setState({
+                                errors: errorData.errors ? errorData.errors : {}
+                            });
+                        }
+                    );
                 }
-            );
+            }
         }
     }
 
     onClickAffiliateLink(e, boId, affId, link) {
         e.preventDefault();
 
-        this.props.trackClick(boId, affId);
+        this.props.makeRequest(request.Track.click, {boId: boId, affId: affId});
 
         setTimeout(function () {
             window.open(link, '_blank');
-        }, 1000);
+        }, 500);
     }
 
 
     render() {
         const {appStatus} = this.props;
         const errors = this.state.errors;
-        const affiliateLinkId = (appStatus.currentBusinessOption.affiliate_links[0]) ? appStatus.currentBusinessOption.affiliate_links[0].id : '';
-        const affiliateLinkLabel = (appStatus.currentBusinessOption.affiliate_links[0]) ? appStatus.currentBusinessOption.affiliate_links[0].label : 'Register for an ABN';
-        const affiliateLink = (appStatus.currentBusinessOption.affiliate_links[0]) ? appStatus.currentBusinessOption.affiliate_links[0].link : '#';
+        const affiliateLinkId = (appStatus.currentBusinessOption.affiliateLinks[0]) ? appStatus.currentBusinessOption.affiliateLinks[0].id : '';
+        const affiliateLinkLabel = (appStatus.currentBusinessOption.affiliateLinks[0]) ? appStatus.currentBusinessOption.affiliateLinks[0].label : 'Find me a web address';
+        const affiliateLink = (appStatus.currentBusinessOption.affiliateLinks[0]) ? appStatus.currentBusinessOption.affiliateLinks[0].link : '#';
+
         return (
             <form className="apps-form" onSubmit={this.onSubmit}>
-                <TextFieldGroup
-                    error={errors.abn}
-                    label="Enter your Australian Business Number (ABN) *"
-                    placeholder="eg. 3522 2525 2524"
-                    onChange={this.onChange}
-                    value={(this.state.abn) ? this.state.abn : appStatus.abn}
-                    type="text"
-                    field="abn"
-                />
+                <TextFieldGroup fieldObject={this.state.abn} onChange={this.onChange}
+                                error={errors.abn}/>
 
                 <span className="find-web-span">Don’t have a ABN?</span>
                 <a onClick={(e) => this.onClickAffiliateLink(e, appStatus.currentBusinessOption.id, affiliateLinkId, affiliateLink)}
@@ -150,7 +171,7 @@ class RegisterBusiness extends Component {
                    className="btn btn-lg btn-default clearfix btn-level-5">{affiliateLinkLabel}</a>
 
                 <div className="btn-wrap">
-                    <button disabled={this.state.isLoading} className="btn btn-default btn-md">Done</button>
+                    <button className="btn btn-default btn-md">Continue</button>
                 </div>
             </form>
         );
@@ -163,13 +184,8 @@ RegisterBusiness.propTypes = {
     saveBusinessFormRequest: PropTypes.func.isRequired,
     addFlashMessage: PropTypes.func.isRequired,
     getBusinessOptionFromUrl: PropTypes.func.isRequired,
-    getAppStatus: PropTypes.func.isRequired,
     getBusinessOption: PropTypes.func.isRequired,
-    setShowCompletedPage: PropTypes.func.isRequired,
-    setCompletedStatus: PropTypes.func.isRequired,
-    setCurrentLevel: PropTypes.func.isRequired,
-    setCurrentSection: PropTypes.func.isRequired,
-    setCurrentBusinessOption: PropTypes.func.isRequired,
+    getAppStatus: PropTypes.func.isRequired,
     trackClick: PropTypes.func.isRequired
 };
 
@@ -182,5 +198,6 @@ function mapStateToProps(state) {
 }
 
 export default withRouter(connect(mapStateToProps, {
+    makeRequest,
     addFlashMessage,
 })(RegisterBusiness));
